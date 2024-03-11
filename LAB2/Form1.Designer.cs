bae8a6a19228bd1b59df346855bf9c0dd8af2219
Diana R <romanuk13082006@gmail.com>
2024-03-11 16:46:08 +0200

    Add the project

---
+++ LAB2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LAB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+
+            this.label1 = new System.Windows.Forms.Label();
+            this.Method1 = new System.Windows.Forms.Button();
+            this.Method2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // Method1
+            // 
+            this.Method1.Location = new System.Drawing.Point(70, 96);
+            this.Method1.Name = "Method1";
+            this.Method1.Size = new System.Drawing.Size(242, 50);
+            this.Method1.TabIndex = 2;
+            this.Method1.Text = "Greedy Algorithm";
+            this.Method1.UseVisualStyleBackColor = true;
+            this.Method1.Click += new System.EventHandler(this.Method1_Click);
+
+            this.Method2.Location = new System.Drawing.Point(70, 152);
+            this.Method2.Name = "Method2";
+            this.Method2.Size = new System.Drawing.Size(242, 51);
+            this.Method2.TabIndex = 3;
+            this.Method2.Text = "Chromatic Number Method";
+            this.Method2.UseVisualStyleBackColor = true;
+            this.Method2.Click += new System.EventHandler(this.Method2_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+      
+            this.Controls.Add(this.Method1);
+            this.Controls.Add(this.Method2);        
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Method1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Method2;
+    }
+}
+
---
+++ LAB2/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+namespace LAB2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Method1_Click(object sender, EventArgs e)
+        {
+            var myForm = new Form1();
+            myForm.Show();
+            int v = 11;
+            Graph g = new Graph(v);
+
+            g.AddEdge(0, 3);
+            g.AddEdge(0, 6);
+            g.AddEdge(1, 8);
+            g.AddEdge(1, 10);
+            g.AddEdge(2, 5);
+            g.AddEdge(3, 8);
+            g.AddEdge(4, 7);
+            g.AddEdge(4, 9);
+            g.AddEdge(5, 9);
+  
+            g.AddDirEdge(1, 2);
+            g.AddDirEdge(4, 2);
+            g.AddDirEdge(2, 8);
+            g.AddDirEdge(3, 7);
+            g.AddDirEdge(4, 8);
+            g.AddDirEdge(10, 5);
+
+            StringBuilder textBuilder = new StringBuilder();
+            int edgeCounter = 1;
+            for (int i = 0; i < v; i++)
+            {
+                foreach (var adjVertex in g.adj[i])
+                {
+                    textBuilder.AppendLine($"Ребро {edgeCounter}: ({i + 1}, {adjVertex + 1})");
+                    edgeCounter++;
+                }
+            }
+
+            string text = textBuilder.ToString();
+
+            int numColors = g.GraphColoring(v);
+
+            MessageBox.Show($"Матриця суміжності:\n {text}\nКількість типів крамничок: {numColors}");
+
+        }
+        public class Graph
+        {
+            public List<int>[] adj;
+
+            public Graph(int v)
+            {
+                adj = new List<int>[v];
+                for (int i = 0; i < v; ++i)
+                    adj[i] = new List<int>();
+            }
+
+            public void AddEdge(int v, int w)
+            {
+                adj[v].Add(w);
+                adj[w].Add(v);
+            }
+
+            public void AddDirEdge(int v, int w)
+            {
+                adj[v].Add(w);
+            }
+
+            public int GraphColoring(int v)
+            {
+                int[] color = new int[v + 1];
+
+                for (int i = 0; i < v; i++)
+                {
+                    color[i] = -1;
+                }
+
+                color[0] = 0;
+
+                bool[] appropriate = new bool[v];
+
+                for (int i = 0; i < v; i++)
+                {
+                    appropriate[i] = true;
+                }
+
+                for (int j = 1; j < v; j++)
+                {
+                    foreach (int k in adj[j])
+                    {
+                        if (color[k] != -1)
+                            appropriate[color[k]] = false;
+                    }
+
+
+                    int colorFound;
+                    for (colorFound = 0; colorFound < v; colorFound++)
+                    {
+                        if (appropriate[colorFound])
+                            break;
+                    }
+
+                    color[j] = colorFound;  
+
+                    for (int i = 0; i < v; i++)
+                    {
+                        appropriate[i] = true;
+                    }
+                }
+
+                HashSet<int> uniqueColors = new HashSet<int>(color);
+                int numColors = uniqueColors.Count + 1;
+                return numColors;
+
+            }
+
+        }
+        private void Method2_Click(object sender, EventArgs e)
+        {
+            var myForm = new Form1();
+            myForm.Show();
+            int v = 11;
+            Graph g2 = new Graph(v);
+            g2.AddEdge(0, 3);
+            g2.AddEdge(0, 6);
+            g2.AddEdge(1, 8);
+            g2.AddEdge(1, 10);
+            g2.AddEdge(2, 5);
+            g2.AddEdge(3, 8);
+            g2.AddEdge(4, 7);
+            g2.AddEdge(4, 9);
+            g2.AddEdge(5, 9);
+
+            g2.AddDirEdge(1, 2);
+            g2.AddDirEdge(4, 2);
+            g2.AddDirEdge(2, 8);
+            g2.AddDirEdge(3, 7);
+            g2.AddDirEdge(4, 8);
+            g2.AddDirEdge(10, 5);
+
+            int numColors = DSatur(v, g2.adj);
+
+            StringBuilder textBuilder = new StringBuilder();
+            int edgeCounter = 1;
+            for (int i = 0; i < v; i++)
+            {
+                foreach (var adjVertex in g2.adj[i])
+                {
+                    textBuilder.AppendLine($"Ребро {edgeCounter}: ({i + 1}, {adjVertex + 1})");
+                    edgeCounter++;
+                }
+            }
+            string text = textBuilder.ToString();
+            MessageBox.Show($"Матриця суміжності:\n {text}\nКількість типів крамничок: {numColors}");
+
+
+        }
+
+        public int DSatur(int v, List<int>[] adj)
+        {
+            int[] colors = new int[v];
+            for (int i = 0; i < v; i++)
+            {
+                colors[i] = -1;
+            }
+
+            for (int j = 0; j < v; j++)
+            {
+                HashSet<int> usedColors = new HashSet<int>();
+
+                foreach (int neighbor in adj[j])
+                {
+                    if (colors[neighbor] != -1)
+                    {
+                        usedColors.Add(colors[neighbor]);
+                    }
+                }
+
+                int color;
+                for (color = 1; ; color++)
+                {
+                    if (!usedColors.Contains(color))
+                    {
+                        colors[j] = color;
+                        break;
+                    }
+                }
+            }
+            int chromaticNumber = colors.Max();
+            return chromaticNumber + 1;
+        }
+    }
+
+
+}
